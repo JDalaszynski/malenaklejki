ed15cef9efa672a0f3b8d6d5b9844799786eef59
JDalaszynski <jakub.dalaszynski@gmail.com>
2026-07-06 19:11:07 +0200

    update

--- test_action.ts
+++ test_action.ts
@@ -2,7 +2,7 @@ import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
 
 // Mock next/headers
-jest = require('jest-mock');
+const jest = require('jest-mock');
 jest.mock('next/headers', () => ({
   headers: () => new Map([['x-forwarded-for', '127.0.0.1']]),
 }));
